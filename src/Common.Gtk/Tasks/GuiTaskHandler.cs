f3e8b7ecda1e178eaec004a36cdd3468b5fc9d91
Bastian Eicher <bastian@eicher.net>
2014-05-08 21:38:32 +0200

    Common.Gtk: Created GuiTaskHandler

---
+++ src/Common.Gtk/Tasks/GuiTaskHandler.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * Copyright 2006-2014 Bastian Eicher
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using System;
+using Gtk;
+
+namespace NanoByte.Common.Tasks
+{
+    /// <summary>
+    /// Uses simple dialog boxes to inform the user about the progress of tasks.
+    /// </summary>
+    public class GuiTaskHandler : MarshalNoTimeout, ITaskHandler
+    {
+        private readonly Window _owner;
+
+        /// <summary>
+        /// Creates a new task handler.
+        /// </summary>
+        /// <param name="owner">The parent window for any dialogs created by the handler.</param>
+        public GuiTaskHandler(Window owner = null)
+        {
+            _owner = owner;
+        }
+
+        protected readonly CancellationTokenSource CancellationTokenSource = new CancellationTokenSource();
+
+        /// <inheritdoc/>
+        public CancellationToken CancellationToken { get { return CancellationTokenSource.Token; } }
+
+        /// <inheritdoc/>
+        public virtual void RunTask(ITask task)
+        {
+            #region Sanity checks
+            if (task == null) throw new ArgumentNullException("task");
+            #endregion
+
+            using (var dialog = new TaskRunDialog(task, CancellationTokenSource, _owner))
+            {
+                dialog.Execute();
+                if (dialog.Exception != null) dialog.Exception.Rethrow();
+            }
+        }
+
+        /// <summary>
+        /// Always returns 1. This ensures that information hidden by the GUI is at least retrievable from the log files.
+        /// </summary>
+        public virtual int Verbosity { get { return 1; } set {} }
+
+        /// <inheritdoc/>
+        public bool Batch { get; set; }
+
+        /// <inheritdoc/>
+        public virtual bool AskQuestion(string question, string batchInformation = null)
+        {
+            if (Batch)
+            {
+                if (!string.IsNullOrEmpty(batchInformation)) Log.Warn(batchInformation);
+                return false;
+            }
+
+            return Msg.OKCancel(_owner, question, MsgSeverity.Warn);
+        }
+
+        /// <inheritdoc/>
+        public virtual void Output(string title, string information)
+        {
+            Msg.Inform(_owner, title + Environment.NewLine + information, MsgSeverity.Warn);
+        }
+
+        #region Dispose
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing) CancellationTokenSource.Dispose();
+        }
+        #endregion
+    }
+}
---
+++ src/Common.Gtk/Tasks/TaskRunDialog.cs
@@ -0,0 +1,115 @@
+﻿/*
+ * Copyright 2006-2014 Bastian Eicher
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using System;
+using System.Threading;
+using System.Diagnostics.CodeAnalysis;
+using Gtk;
+
+namespace NanoByte.Common.Tasks
+{
+    /// <summary>
+    /// A dialog with a progress bar that runs and tracks the progress of an <see cref="ITask"/>.
+    /// </summary>
+    internal partial class TaskRunDialog : Dialog
+    {
+        private readonly ITask _task;
+        private readonly Thread _taskThread;
+        private readonly CancellationTokenSource _cancellationTokenSource;
+        private readonly Progress<TaskSnapshot> _progress = new Progress<TaskSnapshot>();
+
+        /// <summary>
+        /// Creates a new task tracking dialog.
+        /// </summary>
+        /// <param name="task">The trackable task to execute and display.</param>
+        /// <param name="cancellationTokenSource">Used to signal if the user pressed the Cancel button.</param>
+        /// <param name="parent">The parent window for this dialog, if any.</param>
+        public TaskRunDialog(ITask task, CancellationTokenSource cancellationTokenSource, Window parent = null)
+        {
+            #region Sanity checks
+            if (task == null) throw new ArgumentNullException("task");
+            #endregion
+
+            _task = task;
+            _taskThread = new Thread(RunTask);
+            _cancellationTokenSource = cancellationTokenSource;
+
+            Parent = parent;
+            // ReSharper disable once DoNotCallOverridableMethodsInConstructor
+            Build();
+            Title = task.Name;
+            buttonCancel.Sensitive = task.CanCancel;
+        }
+
+        /// <summary>
+        /// Displays the dialog and runs the task. Handles cancellation and closing and destroying the dialog.
+        /// </summary>
+        public void Execute()
+        {
+            _progress.ProgressChanged += OnProgressChanged;
+
+            _taskThread.Start();
+
+            Run();
+
+            while (_taskThread.IsAlive && Exception == null) // Only close the window if the task is no longer running
+            {
+
+                if (_task.CanCancel)
+                {
+                    buttonCancel.Sensitive = false; // Don't tempt user to press Cancel again
+                    _cancellationTokenSource.Cancel();
+                }
+
+                Run();
+            }
+
+            Destroy();
+        }
+
+        private void OnProgressChanged(TaskSnapshot snapshot)
+        {
+            if (snapshot.Status == TaskStatus.Complete) OnClose();
+            progressBarTask.Fraction = snapshot.Value;
+            labelTask.Text = snapshot.ToString();
+        }
+
+        /// <summary>
+        /// An exception thrown by <see cref="ITask.Run"/>, if any.
+        /// </summary>
+        public Exception Exception { get; private set; }
+
+        [SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "Exceptions are rethrown on the UI thread.")]
+        private void RunTask()
+        {
+            try
+            {
+                _task.Run(_cancellationTokenSource.Token, _progress);
+            }
+            catch (Exception ex)
+            {
+                Exception = ex;
+                Application.Invoke(delegate { OnClose(); });
+            }
+        }
+    }
+}
