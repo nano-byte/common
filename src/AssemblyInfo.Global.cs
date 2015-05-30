ab5a441fa38ac95748a4d5e4518b3fddd75e48aa
Bastian Eicher <bastian@eicher.net>
2015-05-30 17:52:39 +0200

    Updated copyright headers

--- src/AssemblyInfo.Global.cs
+++ src/AssemblyInfo.Global.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 // Assembly info
 [assembly: AssemblyCompany("NanoByte")]
 [assembly: AssemblyProduct("NanoByte.Common")]
-[assembly: AssemblyCopyright("Copyright 2006-2014 Bastian Eicher et al")]
+[assembly: AssemblyCopyright("Copyright 2006-2015 Bastian Eicher et al")]
 
 // Version information
 [assembly: AssemblyVersion("1.2.0")]
