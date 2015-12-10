d5982e57a7b9d38f4161b93c658c92fd7dbd5de6
Bastian Eicher <bastian@eicher.net>
2015-12-10 17:55:27 +0100

    Moved some code from WindowsTaskbar to WindowsTaskbar.NativeMethods

--- src/Common/Native/WindowsTaskbar.cs
+++ src/Common/Native/WindowsTaskbar.cs
@@ -25,7 +25,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
-using System.Text;
 using JetBrains.Annotations;
 
 namespace NanoByte.Common.Native
@@ -36,7 +35,6 @@ namespace NanoByte.Common.Native
     [SuppressMessage("ReSharper", "SuspiciousTypeConversion.Global"), SuppressMessage("ReSharper", "UnusedMember.Local"), SuppressMessage("ReSharper", "EmptyGeneralCatchClause")]
     public static partial class WindowsTaskbar
     {
-        #region Enumerations
         /// <summary>
         /// Represents the thumbnail progress bar state.
         /// </summary>
@@ -69,43 +67,6 @@ namespace NanoByte.Common.Native
             Paused = 0x8
         }
 
-        private enum ThumbnailMask
-        {
-            Bitmap = 0x1,
-            Icon = 0x2,
-            Tooltip = 0x4,
-            Flags = 0x8
-        }
-
-        [Flags]
-        private enum ThumbnailFlags
-        {
-            Enabled = 0x0,
-            ThbfDisabled = 0x1,
-            ThbfDismissonclick = 0x2,
-            ThbfNobackground = 0x4,
-            ThbfHidden = 0x8,
-            ThbfNoninteractive = 0x10
-        }
-
-        [Flags]
-        private enum StpFlag
-        {
-            None = 0x0,
-            UseThumbnailalways = 0x1,
-            UseThumbnailWhenActive = 0x2,
-            UsePeekAlways = 0x4,
-            UsePeekWhenActive = 0x8
-        }
-
-        private enum KnownDestinationCategory
-        {
-            Frequent = 1,
-            Recent
-        }
-        #endregion
-
-        #region Structures
         /// <summary>
         /// Represents a shell link targeting a file.
         /// </summary>
@@ -174,198 +135,6 @@ namespace NanoByte.Common.Native
                 IconIndex = iconIndex;
             }
         }
-
-        [StructLayout(LayoutKind.Sequential)]
-        [SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Local")]
-        private struct ThumbnailButton
-        {
-            [MarshalAs(UnmanagedType.U4)]
-            private ThumbnailMask dwMask;
-
-            private uint iId;
-            private uint iBitmap;
-            private IntPtr hIcon;
-
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
-            private string szTip;
-
-            [MarshalAs(UnmanagedType.U4)]
-            private ThumbnailFlags dwFlags;
-        }
-        #endregion
-
-        #region COM
-        [ComImport, Guid("c43dc798-95d1-4bea-9030-bb99e2983a1a"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        private interface ITaskbarList4
-        {
-            // ITaskbarList
-            [PreserveSig]
-            void HrInit();
-
-            [PreserveSig]
-            void AddTab(IntPtr hwnd);
-
-            [PreserveSig]
-            void DeleteTab(IntPtr hwnd);
-
-            [PreserveSig]
-            void ActivateTab(IntPtr hwnd);
-
-            [PreserveSig]
-            void SetActiveAlt(IntPtr hwnd);
-
-            // ITaskbarList2
-            [PreserveSig]
-            void MarkFullscreenWindow(IntPtr hwnd, [MarshalAs(UnmanagedType.Bool)] bool fFullscreen);
-
-            // ITaskbarList3
-            [PreserveSig]
-            void SetProgressValue(IntPtr hwnd, UInt64 ullCompleted, UInt64 ullTotal);
-
-            [PreserveSig]
-            void SetProgressState(IntPtr hwnd, ProgressBarState tbpFlags);
-
-            [PreserveSig]
-            void RegisterTab(IntPtr hwndTab, IntPtr hwndMDI);
-
-            [PreserveSig]
-            void UnregisterTab(IntPtr hwndTab);
-
-            [PreserveSig]
-            void SetTabOrder(IntPtr hwndTab, IntPtr hwndInsertBefore);
-
-            [PreserveSig]
-            void SetTabActive(IntPtr hwndTab, IntPtr hwndInsertBefore, uint dwReserved);
-
-            [PreserveSig]
-            uint ThumbBarAddButtons(IntPtr hwnd, uint cButtons, [MarshalAs(UnmanagedType.LPArray)] ThumbnailButton[] pButtons);
-
-            [PreserveSig]
-            uint ThumbBarUpdateButtons(IntPtr hwnd, uint cButtons, [MarshalAs(UnmanagedType.LPArray)] ThumbnailButton[] pButtons);
-
-            [PreserveSig]
-            void ThumbBarSetImageList(IntPtr hwnd, IntPtr himl);
-
-            [PreserveSig]
-            void SetOverlayIcon(IntPtr hwnd, IntPtr hIcon, [MarshalAs(UnmanagedType.LPWStr)] string pszDescription);
-
-            [PreserveSig]
-            void SetThumbnailTooltip(IntPtr hwnd, [MarshalAs(UnmanagedType.LPWStr)] string pszTip);
-
-            [PreserveSig]
-            void SetThumbnailClip(IntPtr hwnd, IntPtr prcClip);
-
-            // ITaskbarList4
-            void SetTabProperties(IntPtr hwndTab, StpFlag stpFlags);
-        }
-
-        [ComImport, Guid("56FDF344-FD6D-11d0-958A-006097C9A090"), ClassInterface(ClassInterfaceType.None)]
-        private class CTaskbarList
-        {}
-
-        [ComImport, Guid("6332DEBF-87B5-4670-90C0-5E57B408A49E"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        private interface ICustomDestinationList
-        {
-            void SetAppID([MarshalAs(UnmanagedType.LPWStr)] string pszAppID);
-
-            [PreserveSig]
-            uint BeginList(out uint cMaxSlots, ref Guid riid, [Out, MarshalAs(UnmanagedType.Interface)] out object ppvObject);
-
-            [PreserveSig]
-            uint AppendCategory([MarshalAs(UnmanagedType.LPWStr)] string pszCategory, [MarshalAs(UnmanagedType.Interface)] IObjectArray poa);
-
-            void AppendKnownCategory([MarshalAs(UnmanagedType.I4)] KnownDestinationCategory category);
-
-            [PreserveSig]
-            uint AddUserTasks([MarshalAs(UnmanagedType.Interface)] IObjectArray poa);
-
-            void CommitList();
-            void GetRemovedDestinations(ref Guid riid, [Out, MarshalAs(UnmanagedType.Interface)] out object ppvObject);
-            void DeleteList([MarshalAs(UnmanagedType.LPWStr)] string pszAppID);
-            void AbortList();
-        }
-
-        [ComImport, Guid("77F10CF0-3DB5-4966-B520-B7C54FD35ED6"), ClassInterface(ClassInterfaceType.None)]
-        private class CDestinationList
-        {}
-
-        [ComImport, Guid("92CA9DCD-5622-4BBA-A805-5E9F541BD8C9"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        private interface IObjectArray
-        {
-            void GetCount(out uint cObjects);
-            void GetAt(uint iIndex, ref Guid riid, [Out, MarshalAs(UnmanagedType.Interface)] out object ppvObject);
-        }
-
-        [ComImport, Guid("5632B1A4-E38A-400A-928A-D4CD63230295"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        private interface IObjectCollection
-        {
-            // IObjectArray
-            [PreserveSig]
-            void GetCount(out uint cObjects);
-
-            [PreserveSig]
-            void GetAt(uint iIndex, ref Guid riid, [Out, MarshalAs(UnmanagedType.Interface)] out object ppvObject);
-
-            // IObjectCollection
-            void AddObject([MarshalAs(UnmanagedType.Interface)] object pvObject);
-            void AddFromArray([MarshalAs(UnmanagedType.Interface)] IObjectArray poaSource);
-            void RemoveObject(uint uiIndex);
-            void Clear();
-        }
-
-        [ComImport, Guid("2D3468C1-36A7-43B6-AC24-D3F02FD9607A"), ClassInterface(ClassInterfaceType.None)]
-        private class CEnumerableObjectCollection
-        {}
-
-        [ComImport, Guid("000214F9-0000-0000-C000-000000000046"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        private interface IShellLinkW
-        {
-            void GetPath([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszFile, int cchMaxPath, IntPtr pfd, uint fFlags);
-            void GetIDList(out IntPtr ppidl);
-            void SetIDList(IntPtr pidl);
-            void GetDescription([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszFile, int cchMaxName);
-            void SetDescription([MarshalAs(UnmanagedType.LPWStr)] string pszName);
-            void GetWorkingDirectory([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszDir, int cchMaxPath);
-            void SetWorkingDirectory([MarshalAs(UnmanagedType.LPWStr)] string pszDir);
-            void GetArguments([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszArgs, int cchMaxPath);
-            void SetArguments([MarshalAs(UnmanagedType.LPWStr)] string pszArgs);
-            void GetHotKey(out short wHotKey);
-            void SetHotKey(short wHotKey);
-            void GetShowCmd(out uint iShowCmd);
-            void SetShowCmd(uint iShowCmd);
-            void GetIconLocation([Out, MarshalAs(UnmanagedType.LPWStr)] out StringBuilder pszIconPath, int cchIconPath, out int iIcon);
-            void SetIconLocation([MarshalAs(UnmanagedType.LPWStr)] string pszIconPath, int iIcon);
-            void SetRelativePath([MarshalAs(UnmanagedType.LPWStr)] string pszPathRel, uint dwReserved);
-            void Resolve(IntPtr hwnd, uint fFlags);
-            void SetPath([MarshalAs(UnmanagedType.LPWStr)] string pszFile);
-        }
-
-        [ComImport, Guid("00021401-0000-0000-C000-000000000046"), ClassInterface(ClassInterfaceType.None)]
-        private class CShellLink
-        {}
-
-        private static readonly ITaskbarList4 _taskbarList;
-
-        [SuppressMessage("Microsoft.Performance", "CA1810:InitializeReferenceTypeStaticFieldsInline", Justification = "Must perform COM call during init")]
-        [SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "COM calls throw unpredictable exceptions and this methods successful execution is not critical.")]
-        static WindowsTaskbar()
-        {
-            if (WindowsUtils.IsWindows7)
-            {
-                try
-                {
-                    _taskbarList = (ITaskbarList4)new CTaskbarList();
-                    _taskbarList.HrInit();
-                }
-                    #region Error handling
-                catch (Exception ex)
-                {
-                    Log.Warn(ex);
-                }
-                #endregion
-            }
-        }
-        #endregion
 
         /// <summary>
         /// Sets the state of the taskbar progress indicator.
