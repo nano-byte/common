d01dcba335a8d6f5d2a2c0e6dd00b5e3a1c750ca
Bastian Eicher <bastian@eicher.net>
2016-03-25 19:45:28 +0100

    Added Common.WinForms.Sample project

---
+++ src/Common.WinForms.Sample/SampleForm.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace Common.WinForms.Sample
+{
+    public partial class SampleForm : Form
+    {
+        public SampleForm()
+        {
+            InitializeComponent();
+
+            resettablePropertyGrid.SelectedObject = taskControl;
+        }
+    }
+}
