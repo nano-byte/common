b78164f084265332bdc4fa72db5d02bfbd23b650
Bastian Eicher <bastian@eicher.net>
2016-06-01 14:56:44 +0200

    Added DictionaryExtensions.GetOrAdd() and .GetOrAddAsync()

--- src/Common/Collections/DictionaryExtensions.cs
+++ src/Common/Collections/DictionaryExtensions.cs
@@ -24,6 +24,10 @@ using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 
+#if NET45
+using System.Threading.Tasks;
+#endif
+
 namespace NanoByte.Common.Collections
 {
     /// <summary>
@@ -46,5 +50,52 @@ namespace NanoByte.Common.Collections
             foreach (var pair in source)
                 target.Add(pair.Key, pair.Value);
         }
+
+        /// <summary>
+        /// Returns the element with the specified <paramref name="key"/> from the <paramref name="dictionary"/>.
+        /// Creates, adds and returns a new value using <paramref name="valueFactory"/> if no match was found.
+        /// </summary>
+        public static TValue GetOrAdd<TKey, TValue>([NotNull] this IDictionary<TKey, TValue> dictionary, [NotNull] TKey key, [NotNull, InstantHandle] Func<TValue> valueFactory)
+        {
+            #region Sanity checks
+            if (dictionary == null) throw new ArgumentNullException("dictionary");
+            if (key == null) throw new ArgumentNullException("key");
+            if (valueFactory == null) throw new ArgumentNullException("valueFactory");
+            #endregion
+
+            TValue value;
+            if (!dictionary.TryGetValue(key, out value))
+                dictionary.Add(key, value = valueFactory());
+            return value;
+        }
+
+        /// <summary>
+        /// Returns the element with the specified <paramref name="key"/> from the <paramref name="dictionary"/>.
+        /// Creates, adds and returns a new <typeparamref name="TValue"/> instance if no match was found.
+        /// </summary>
+        public static TValue GetOrAdd<TKey, TValue>([NotNull] this IDictionary<TKey, TValue> dictionary, [NotNull] TKey key)
+            where TValue : new()
+        {
+            #region Sanity checks
+            if (dictionary == null) throw new ArgumentNullException("dictionary");
+            if (key == null) throw new ArgumentNullException("key");
+            #endregion
+
+            return dictionary.GetOrAdd(key, () => new TValue());
+        }
+
+#if NET45
+        /// <summary>
+        /// Returns the element with the specified <paramref name="key"/> from the <paramref name="dictionary"/>.
+        /// Creates, adds and returns a new value using <paramref name="valueFactory"/> if no match was found.
+        /// </summary>
+        public static async Task<TValue> GetOrAddAsync<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<Task<TValue>> valueFactory)
+        {
+            TValue value;
+            if (!dictionary.TryGetValue(key, out value))
+                dictionary.Add(key, value = await valueFactory());
+            return value;
+        }
+#endif
     }
 }
