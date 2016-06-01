7d5bc2e9fa944f2ad8a3b83770030cd123c6b33e
Bastian Eicher <bastian@eicher.net>
2016-06-01 16:11:51 +0200

    Started development on v2.0.1

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -8,4 +8,4 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright 2006-2015 Bastian Eicher et al")]
 
 // Version information
-[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.1")]
