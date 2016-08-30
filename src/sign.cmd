d32047c9d8f4f69c0369f16c30a299ce2bc4f416
Bastian Eicher <bastian@eicher.net>
2016-08-30 20:07:51 +0200

    Use sha256 for AuthentiCode

--- src/sign.cmd
+++ src/sign.cmd
@@ -1,7 +1,7 @@
 @echo off
 ::Adds AuthentiCode signatures to all binaries. Assumes "build.cmd Release" has already been executed.
 if not "%1" == "" set signing_cert_path=%*
-set timestamp_server=http://timestamp.comodoca.com/authenticode
+set timestamp_server=http://timestamp.globalsign.com/?signature=sha2
 
 rem Determine VS version
 if defined VS140COMNTOOLS (
@@ -16,8 +16,8 @@ exit /b 1
 
 
 echo Signing binaries with "%signing_cert_path%"...
-FOR %%A IN ("%~dp0..\build\Release\NanoByte.Common*.dll") DO signtool sign /t %timestamp_server% /f "%signing_cert_path%" /p "%signing_cert_pass%" /v "%%A"
-FOR %%A IN ("%~dp0..\build\ReleaseNet40\NanoByte.Common*.dll") DO signtool sign /t %timestamp_server% /f "%signing_cert_path%" /p "%signing_cert_pass%" /v "%%A"
-FOR %%A IN ("%~dp0..\build\ReleaseNet35\NanoByte.Common*.dll") DO signtool sign /t %timestamp_server% /f "%signing_cert_path%" /p "%signing_cert_pass%" /v "%%A"
-FOR %%A IN ("%~dp0..\build\ReleaseNet20\NanoByte.Common*.dll") DO signtool sign /t %timestamp_server% /f "%signing_cert_path%" /p "%signing_cert_pass%" /v "%%A"
+FOR %%A IN ("%~dp0..\build\Release\NanoByte.Common*.dll") DO signtool sign /f "%signing_cert_path%" /fd sha256 /p "%signing_cert_pass%" /tr %timestamp_server% /td sha256 /q "%%A"
+FOR %%A IN ("%~dp0..\build\ReleaseNet40\NanoByte.Common*.dll") DO signtool sign /f "%signing_cert_path%" /fd sha256 /p "%signing_cert_pass%" /tr %timestamp_server% /td sha256 /q "%%A"
+FOR %%A IN ("%~dp0..\build\ReleaseNet35\NanoByte.Common*.dll") DO signtool sign /f "%signing_cert_path%" /fd sha256 /p "%signing_cert_pass%" /tr %timestamp_server% /td sha256 /q "%%A"
+FOR %%A IN ("%~dp0..\build\ReleaseNet20\NanoByte.Common*.dll") DO signtool sign /f "%signing_cert_path%" /fd sha256 /p "%signing_cert_pass%" /tr %timestamp_server% /td sha256 /q "%%A"
 if errorlevel 1 exit /b %errorlevel%
