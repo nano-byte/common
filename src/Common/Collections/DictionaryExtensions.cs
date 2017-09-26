d01d7aa226ede319e1d40d379f1bd349c0066790
Bastian Eicher <bastian@eicher.net>
2017-09-26 15:48:41 +0200

    Added tuple deconstructor for KeyValuePair

--- src/Common/Collections/DictionaryExtensions.cs
+++ src/Common/Collections/DictionaryExtensions.cs
@@ -187,5 +187,19 @@ namespace NanoByte.Common.Collections
                 return result;
             }
         }
+
+        /// <summary>
+        /// Deconstructs a <see cref="KeyValuePair{TKey,TValue}"/> like a tuple.
+        /// </summary>
+        /// <example>
+        /// foreach (var (key, value) in dictionary)
+        /// {/*...*/}
+        /// </example>
+        [Pure]
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> pair, out TKey key, out TValue value)
+        {
+            key = pair.Key;
+            value = pair.Value;
+        }
     }
 }
