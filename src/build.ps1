858fc7050601249c6f4e8a1a55b785254eae820d
Bastian Eicher <bastian@eicher.net>
2017-11-30 01:56:16 +0100

    Improved build script and readme

--- src/build.ps1
+++ src/build.ps1
@@ -1,8 +1,10 @@
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+$vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath -format value
+$msBuild = "$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"
 
-dotnet restore
-. "$(. "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath -format value)\Common7\IDE\devenv.com" NanoByte.Common.sln /Build Release
+. $msBuild /v:Quiet /t:Clean
+. $msBuild /v:Quiet /t:Restore /t:Build /p:Configuration=Release
 dotnet test --configuration Release --no-build Common.UnitTests\Common.UnitTests.csproj
 dotnet test --configuration Release --no-build Common.SlimDX.UnitTests\Common.SlimDX.UnitTests.csproj
 
