269373666a5c1c38a69a5abc5301dbe07c74cffa
Bastian Eicher <bastian@eicher.net>
2018-03-16 13:24:40 +0100

    Simplified building without Visual Studio

--- src/test.ps1
+++ src/test.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-dotnet test --configuration Release --no-build Common.UnitTests\Common.UnitTests.csproj
-dotnet test --configuration Release --no-build Common.SlimDX.UnitTests\Common.SlimDX.UnitTests.csproj
+dotnet test --no-build --configuration Release Common.UnitTests\Common.UnitTests.csproj
+dotnet test --no-build --configuration Release Common.SlimDX.UnitTests\Common.SlimDX.UnitTests.csproj
 
 popd
--- src/test.sh
+++ src/test.sh
@@ -2,5 +2,4 @@
 set -e
 cd `dirname $0`
 
-dotnet test --configuration Release --no-build Common.UnitTests/Common.UnitTests.csproj
-dotnet test --configuration Release --no-build Common.SlimDX.UnitTests/Common.SlimDX.UnitTests.csproj
+dotnet test --no-build Common.UnitTests/Common.UnitTests.csproj
