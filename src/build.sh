33ead265b9bb138f1ed3e4bd986589a855a248c5
Bastian Eicher <bastian@eicher.net>
2018-03-17 10:27:14 +0100

    Switched versioning to GitVersion

--- src/build.sh
+++ src/build.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 
 echo "WARNING: You need Visual Studio on Windows to perform a full Release build"
 if hash msbuild 2>/dev/null; then
-    msbuild -t:Restore -t:Build -p:Configuration=DebugLinuxMono
+    msbuild -t:Restore -t:Build -p:Configuration=DebugLinuxMono -p:Version=${1:-1.0-dev}
 else
-    dotnet msbuild -t:Restore -t:Build -p:Configuration=DebugLinuxCore
+    dotnet msbuild -t:Restore -t:Build -p:Configuration=DebugLinuxCore -p:Version=${1:-1.0-dev}
 fi
