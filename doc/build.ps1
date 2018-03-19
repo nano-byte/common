8175ad95fcdada0fc453c561470330488df4b7d9
Bastian Eicher <bastian@eicher.net>
2018-03-19 19:30:04 +0100

    Build Doxygen docs on AppVeyor

--- doc/build.ps1
+++ doc/build.ps1
@@ -1,8 +1,16 @@
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
+if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
+    # Put 0install in PATH using Bootstrapper
+    mkdir -Force "$env:TEMP\zero-install" | Out-Null
+    Invoke-WebRequest "https://0install.de/files/zero-install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
+    $env:PATH = "$env:TEMP\zero-install;$env:PATH"
+}
+
 if (Test-Path ..\build\Documentation) {rm -Recurse -Force ..\build\Documentation}
 mkdir ..\build\Documentation | Out-Null
-0install run http://0install.de/feeds/Doxygen.xml
+
+0install run --batch http://0install.de/feeds/Doxygen.xml
 
 popd
--- doc/build.sh
+++ doc/build.sh
@@ -4,4 +4,4 @@ cd `dirname $0`
 
 rm -rf ../build/Documentation
 mkdir -p ../build/Documentation
-0install run http://0install.de/feeds/Doxygen.xml
+0install run --batch http://0install.de/feeds/Doxygen.xml
