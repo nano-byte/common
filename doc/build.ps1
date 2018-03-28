31feac2fe8e97f2540c19bcccaa24e29674dcbec
Bastian Eicher <bastian@eicher.net>
2018-03-29 01:24:58 +0200

    Tweaked build

--- doc/build.ps1
+++ doc/build.ps1
@@ -1,16 +1,16 @@
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
+# Ensure 0install is in the PATH
 if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
-    # Put 0install in PATH using Bootstrapper
     mkdir -Force "$env:TEMP\zero-install" | Out-Null
-    Invoke-WebRequest "https://0install.de/files/zero-install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
+    Invoke-WebRequest "https://0install.de/files/0install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
     $env:PATH = "$env:TEMP\zero-install;$env:PATH"
 }
 
 if (Test-Path ..\build\Documentation) {rm -Recurse -Force ..\build\Documentation}
 mkdir ..\build\Documentation | Out-Null
 
-0install run --batch http://0install.de/feeds/Doxygen.xml
+cmd /c "0install run --batch http://0install.de/feeds/Doxygen.xml 2>&1" # Redirect stderr to stdout
 
 popd
