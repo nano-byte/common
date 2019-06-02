9024e2963d171e0d0d3fb28f542704bbfe495cb3
Bastian Eicher <bastian@eicher.net>
2019-06-02 22:33:05 +0200

    Tweaked .csproj files

--- src/build.sh
+++ src/build.sh
@@ -6,7 +6,7 @@ echo "WARNING: You need Visual Studio on Windows to perform a full Release build
 
 # Prefer Mono's msbuild over .NET Core's
 if hash msbuild 2>/dev/null; then
-    msbuild -v:Quiet -t:Restore -t:Build -p:Version=${1:-1.0-dev}
+    msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Debug -p:Version=${1:-1.0-dev}
 else
     dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=DebugLinuxCore -p:Version=${1:-1.0-dev}
 fi
