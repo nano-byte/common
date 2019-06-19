3aca78f1e2e891aa597b934c52e3452e4219add4
Bastian Eicher <bastian@eicher.net>
2019-06-19 12:23:50 +0200

    Disabled Mono build due to bug in integration with current .NET Core SDK versions

--- src/build.sh
+++ src/build.sh
@@ -5,8 +5,8 @@ cd `dirname $0`
 echo "WARNING: You need Visual Studio on Windows to perform a full Release build"
 
 # Prefer Mono's msbuild over .NET Core's
-if hash msbuild 2>/dev/null; then
-    msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Debug -p:Version=${1:-1.0-dev}
-else
+#if hash msbuild 2>/dev/null; then
+#    msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Debug -p:Version=${1:-1.0-dev}
+#else
     dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=DebugLinuxCore -p:Version=${1:-1.0-dev}
-fi
+#fi
