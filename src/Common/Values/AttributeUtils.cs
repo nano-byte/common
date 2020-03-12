65686937dc90e1a23a34d09460df4562f83428f7
Bastian Eicher <bastian@eicher.net>
2020-03-12 01:56:18 +0100

    Tweaked AttributeUtils

--- src/Common/Values/AttributeUtils.cs
+++ src/Common/Values/AttributeUtils.cs
@@ -33,10 +33,8 @@ namespace NanoByte.Common.Values
         /// <returns>Falls back to <see cref="object.ToString"/> if the attribute is missing.</returns>
         public static string GetEnumAttributeValue<TAttribute>(this Enum target, Converter<TAttribute, string> valueRetriever) where TAttribute : Attribute
         {
-            #region Sanity checks
             if (target == null) throw new ArgumentNullException(nameof(target));
             if (valueRetriever == null) throw new ArgumentNullException(nameof(valueRetriever));
-            #endregion
 
             var fieldInfo = target.GetType().GetField(target.ToString());
             var attributes = (TAttribute[])fieldInfo.GetCustomAttributes(typeof(TAttribute), inherit: true);
@@ -48,27 +46,18 @@ namespace NanoByte.Common.Values
         /// Uses the type converter for <typeparamref name="TType"/> (set by <see cref="TypeConverterAttribute"/>) to parse a string.
         /// </summary>
         public static TType ConvertFromString<TType>(this string value)
-        {
-            #region Sanity checks
-            if (value == null) throw new ArgumentNullException(nameof(value));
-            #endregion
-
-            return (TType)(TypeDescriptor.GetConverter(typeof(TType)).ConvertFromInvariantString(value));
-        }
+            => (TType)TypeDescriptor
+                     .GetConverter(typeof(TType))
+                     .ConvertFromInvariantString(value ?? throw new ArgumentNullException(nameof(value)));
 
         /// <summary>
         /// Uses the type converter for <typeparamref name="TType"/> (set by <see cref="TypeConverterAttribute"/>) to generate a string.
         /// </summary>
         public static string ConvertToString<TType>(this TType value)
-        {
-            #region Sanity checks
-            // ReSharper disable CompareNonConstrainedGenericWithNull
-            if (value == null) throw new ArgumentNullException(nameof(value));
-            // ReSharper restore CompareNonConstrainedGenericWithNull
-            #endregion
-
-            return TypeDescriptor.GetConverter(typeof(TType)).ConvertToInvariantString(value) ?? "";
-        }
+            => TypeDescriptor
+              .GetConverter(typeof(TType))
+              .ConvertToInvariantString(value ?? throw new ArgumentNullException(nameof(value)))
+            ?? "";
 
         /// <summary>
         /// Retrieves a single value from a Custom <see cref="Attribute"/> associated with an <see cref="Assembly"/>.
@@ -78,16 +67,17 @@ namespace NanoByte.Common.Values
         /// <param name="assembly">The <see cref="Assembly"/> to retrieve the <typeparamref name="TAttribute"/> from.</param>
         /// <param name="valueRetrieval">A callback used to retrieve a <typeparamref name="TValue"/> from a <typeparamref name="TAttribute"/>.</param>
         /// <returns>The retrieved value or <c>null</c> if no <typeparamref name="TAttribute"/> was found.</returns>
-        public static TValue GetAttributeValue<TAttribute, TValue>(this Assembly assembly, Func<TAttribute, TValue> valueRetrieval)
+        public static TValue? GetAttributeValue<TAttribute, TValue>(this Assembly assembly, Func<TAttribute, TValue?> valueRetrieval)
             where TAttribute : Attribute
+            where TValue : class
         {
-            #region Sanity checks
             if (assembly == null) throw new ArgumentNullException(nameof(assembly));
             if (valueRetrieval == null) throw new ArgumentNullException(nameof(valueRetrieval));
-            #endregion
 
             var attributes = assembly.GetCustomAttributes(typeof(TAttribute), inherit: false);
-            return (attributes.Length > 0) ? valueRetrieval((TAttribute)attributes[0]) : default;
+            return (attributes.Length > 0)
+                ? valueRetrieval((TAttribute)attributes[0])
+                : default;
         }
     }
 }
