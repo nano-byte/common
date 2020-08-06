a0514af32d8d3bea537722346815454de78e437a
Bastian Eicher <bastian@eicher.net>
2020-08-06 19:38:49 +0200

    Improved build scripts

--- src/build.ps1
+++ src/build.ps1
@@ -2,15 +2,37 @@
 $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
-if (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe") {
-  $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property installationPath -format value -version 16.5
-  $msBuild = if (Test-Path "$vsDir\MSBuild\Current") {"$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe"} else {"$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"}
-} else {
-  throw "Needs Visual Studio 2019 v16.5 or newer"
+function Find-MSBuild {
+    if (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe") {
+        $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property installationPath -format value -version 16.5
+        if ($vsDir) {
+            if (Test-Path "$vsDir\MSBuild\Current") { return "$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe" } else { return "$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe" }
+        }
+    }
 }
 
-if ($env:CI) { $ci = "-p:ContinuousIntegrationBuild=True" }
-. $msBuild -v:Quiet -t:Restore -t:Build $ci -p:Configuration=Release -p:Version=$Version
-if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+function Run-DotNet {
+    if (Get-Command dotnet -ErrorAction SilentlyContinue) {
+        dotnet @args
+    } else {
+        ..\_0install.ps1 run --batch --version 3.1..!3.2 https://apps.0install.net/dotnet/core-sdk.xml @args
+    }
+    if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+}
+
+function Run-MSBuild {
+    $msbuild = Find-MSBuild
+    if ($msbuild) {
+        . $msbuild @args
+        if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+    } else {
+        Write-Warning "You need Visual Studio 2019 v16.5+ to perform a full build of this project"
+        Run-DotNet msbuild @args
+    }
+}
+
+# Build
+if ($env:CI) { $ci = "/p:ContinuousIntegrationBuild=True" }
+Run-MSBuild /v:Quiet /t:Restore /t:Build $ci /p:Configuration=Release /p:Version=$Version
 
 popd
--- src/build.sh
+++ src/build.sh
@@ -2,4 +2,15 @@
 set -e
 cd `dirname $0`
 
-dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=${1:-1.0-dev} Common
+echo "WARNING: You need Visual Studio 2019 v16.5+ to perform a full build of this project" >&2
+
+# Find dotnet
+if command -v dotnet > /dev/null 2> /dev/null; then
+    dotnet="dotnet"
+else
+    dotnet="../_0install.sh run --version 3.1..!3.2 https://apps.0install.net/dotnet/core-sdk.xml"
+fi
+
+# Build (without WinForms)
+$dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=${1:-1.0.0-pre} Common
+$dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=${1:-1.0.0-pre} UnitTests
