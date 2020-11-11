3355df67f56a345b6dc01da34564f1245f916592
Bastian Eicher <bastian@eicher.net>
2020-11-11 19:21:43 +0100

    Refactored Msg

--- src/Common.WinForms/Msg.cs
+++ src/Common.WinForms/Msg.cs
@@ -2,9 +2,11 @@
 // Licensed under the MIT License
 
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.Windows.Forms;
+using NanoByte.Common.Native;
 using NanoByte.Common.Properties;
 using TaskDialog;
 
@@ -15,37 +17,20 @@ namespace NanoByte.Common
     /// </summary>
     public static class Msg
     {
-        #region Inform
         /// <summary>
-        /// Displays a message to the user using a <see cref="MessageBox"/> or <see cref="TaskDialog"/>.
+        /// Displays a message to the user using a message box or task dialog.
         /// </summary>
         /// <param name="owner">The parent window the displayed window is modal to; can be <c>null</c>.</param>
         /// <param name="text">The message to be displayed.</param>
         /// <param name="severity">How severe/important the message is.</param>
         public static void Inform(IWin32Window? owner, [Localizable(true)] string text, MsgSeverity severity)
         {
-            if (TaskDialog.TaskDialog.IsAvailable)
-            {
-                try
-                {
-                    ShowTaskDialog(GetTaskDialog(text, severity), owner);
-                }
-                catch (BadImageFormatException)
-                {
-                    ShowMesageBox(owner, text, severity, MessageBoxButtons.OK);
-                }
-                catch (EntryPointNotFoundException)
-                {
-                    ShowMesageBox(owner, text, severity, MessageBoxButtons.OK);
-                }
-            }
-            else ShowMesageBox(owner, text, severity, MessageBoxButtons.OK);
+            if (ShowTaskDialog(owner, text, severity) == null)
+                ShowMessageBox(owner, text, severity, MessageBoxButtons.OK);
         }
-        #endregion
 
-        #region OK/Cancel
         /// <summary>
-        /// Asks the user a OK/Cancel-question using a <see cref="MessageBox"/> or <see cref="TaskDialog"/>.
+        /// Asks the user a OK/Cancel-question using a message box or task dialog.
         /// </summary>
         /// <param name="owner">The parent window the displayed window is modal to; can be <c>null</c>.</param>
         /// <param name="text">The message to be displayed.</param>
@@ -61,53 +46,13 @@ namespace NanoByte.Common
             if (string.IsNullOrEmpty(okCaption)) throw new ArgumentNullException(nameof(okCaption));
             #endregion
 
-            if (TaskDialog.TaskDialog.IsAvailable)
-            {
-                var taskDialog = GetTaskDialog(text, severity);
-
-                // Display default names with custom explanations
-                taskDialog.UseCommandLinks = true;
-
-                if (string.IsNullOrEmpty(cancelCaption))
-                { // Default cancel button
-                    taskDialog.Buttons = new[]
-                    {
-                        new TaskDialogButton((int)DialogResult.OK, okCaption.Replace("\r\n", "\n"))
-                    };
-                    taskDialog.CommonButtons = TaskDialogCommonButtons.Cancel;
-                }
-                else
-                { // Custom cancel button
-                    taskDialog.Buttons = new[]
-                    {
-                        new TaskDialogButton((int)DialogResult.OK, okCaption.Replace("\r\n", "\n")),
-                        new TaskDialogButton((int)DialogResult.Cancel, cancelCaption.Replace("\r\n", "\n"))
-                    };
-                }
-
-                // Only Infos should default to OK
-                if (severity >= MsgSeverity.Warn) taskDialog.DefaultButton = (int)DialogResult.Cancel;
-
-                try
-                {
-                    return ShowTaskDialog(taskDialog, owner) == DialogResult.OK;
-                }
-                #region Error handling
-                catch (BadImageFormatException)
-                {
-                    return ShowMesageBox(owner, text, severity, MessageBoxButtons.OKCancel) == DialogResult.OK;
-                }
-                catch (EntryPointNotFoundException)
-                {
-                    return ShowMesageBox(owner, text, severity, MessageBoxButtons.OKCancel) == DialogResult.OK;
-                }
-                #endregion
-            }
-            else return ShowMesageBox(owner, text, severity, MessageBoxButtons.OKCancel) == DialogResult.OK;
+            var result = ShowTaskDialog(owner, text, severity, ok: okCaption, cancel: cancelCaption, canCancel: true)
+                      ?? ShowMessageBox(owner, text, severity, MessageBoxButtons.OKCancel);
+            return result == DialogResult.OK;
         }
 
         /// <summary>
-        /// Asks the user a OK/Cancel-question using a <see cref="MessageBox"/> or <see cref="TaskDialog"/>.
+        /// Asks the user a OK/Cancel-question using a message box or task dialog.
         /// </summary>
         /// <param name="owner">The parent window the displayed window is modal to; can be <c>null</c>.</param>
         /// <param name="text">The message to be displayed.</param>
@@ -116,11 +61,9 @@ namespace NanoByte.Common
         /// <remarks>If a <see cref="MessageBox"/> is used, OK and Cancel are not display to the user, so don't rely on them!</remarks>
         public static bool OkCancel(IWin32Window? owner, [Localizable(true)] string text, MsgSeverity severity)
             => OkCancel(owner, text, severity, "OK", Resources.Cancel);
-        #endregion
 
-        #region Yes/No
         /// <summary>
-        /// Asks the user to choose between two options (yes/no) using a <see cref="MessageBox"/> or <see cref="TaskDialog"/>.
+        /// Asks the user to choose between two options (yes/no) using a message box or task dialog.
         /// </summary>
         /// <param name="owner">The parent window the displayed window is modal to; can be <c>null</c>.</param>
         /// <param name="text">The message to be displayed.</param>
@@ -137,49 +80,22 @@ namespace NanoByte.Common
             if (string.IsNullOrEmpty(noCaption)) throw new ArgumentNullException(nameof(noCaption));
             #endregion
 
-            if (TaskDialog.TaskDialog.IsAvailable)
-            {
-                var taskDialog = GetTaskDialog(text, severity);
-
-                // Display fully customized text
-                taskDialog.UseCommandLinks = true;
-                taskDialog.Buttons = new[]
-                {
-                    new TaskDialogButton((int)DialogResult.Yes, yesCaption.Replace("\r\n", "\n")),
-                    new TaskDialogButton((int)DialogResult.No, noCaption.Replace("\r\n", "\n"))
-                };
-
-                try
-                {
-                    return (ShowTaskDialog(taskDialog, owner) == DialogResult.Yes);
-                }
-                #region Error handling
-                catch (BadImageFormatException)
-                {
-                    return (ShowMesageBox(owner, text, severity, MessageBoxButtons.YesNo) == DialogResult.Yes);
-                }
-                catch (EntryPointNotFoundException)
-                {
-                    return (ShowMesageBox(owner, text, severity, MessageBoxButtons.YesNo) == DialogResult.Yes);
-                }
-                #endregion
-            }
-            else return (ShowMesageBox(owner, text, severity, MessageBoxButtons.YesNo) == DialogResult.Yes);
+            var result = ShowTaskDialog(owner, text, severity, yes: yesCaption, no: noCaption)
+                      ?? ShowMessageBox(owner, text, severity, MessageBoxButtons.YesNo);
+            return result == DialogResult.Yes;
         }
 
         /// <summary>
-        /// Asks the user to choose between two options (yes/no) using a <see cref="MessageBox"/> or <see cref="TaskDialog"/>.
+        /// Asks the user to choose between two options (yes/no) using a message box or task dialog.
         /// </summary>
         /// <param name="owner">The parent window the displayed window is modal to; can be <c>null</c>.</param>
         /// <param name="text">The message to be displayed.</param>
         /// <param name="severity">How severe/important the message is.</param>
         public static bool YesNo(IWin32Window? owner, [Localizable(true)] string text, MsgSeverity severity)
             => YesNo(owner, text, severity, Resources.Yes, Resources.No);
-        #endregion
 
-        #region Yes/No/Cancel
         /// <summary>
-        /// Asks the user to choose between three options (yes/no/cancel) using a <see cref="MessageBox"/> or <see cref="TaskDialog"/>.
+        /// Asks the user to choose between three options (yes/no/cancel) using a message box or task dialog.
         /// </summary>
         /// <param name="owner">The parent window the displayed window is modal to; can be <c>null</c>.</param>
         /// <param name="text">The message to be displayed.</param>
@@ -198,43 +114,12 @@ namespace NanoByte.Common
             if (string.IsNullOrEmpty(noCaption)) throw new ArgumentNullException(nameof(noCaption));
             #endregion
 
-            if (TaskDialog.TaskDialog.IsAvailable)
-            {
-                var taskDialog = GetTaskDialog(text, severity);
-                taskDialog.AllowDialogCancellation = true;
-                taskDialog.CommonButtons = TaskDialogCommonButtons.Cancel;
-
-                // Display fully customized text
-                taskDialog.UseCommandLinks = true;
-                taskDialog.Buttons = new[]
-                {
-                    new TaskDialogButton((int)DialogResult.Yes, yesCaption.Replace("\r\n", "\n")),
-                    new TaskDialogButton((int)DialogResult.No, noCaption.Replace("\r\n", "\n"))
-                };
-
-                // Infos and Warnings (like Save) should default to yes
-                if (severity >= MsgSeverity.Error) taskDialog.DefaultButton = (int)DialogResult.Cancel;
-
-                try
-                {
-                    return ShowTaskDialog(taskDialog, owner);
-                }
-                #region Error handling
-                catch (BadImageFormatException)
-                {
-                    return ShowMesageBox(owner, text, severity, MessageBoxButtons.YesNoCancel);
-                }
-                catch (EntryPointNotFoundException)
-                {
-                    return ShowMesageBox(owner, text, severity, MessageBoxButtons.YesNoCancel);
-                }
-                #endregion
-            }
-            else return ShowMesageBox(owner, text, severity, MessageBoxButtons.YesNoCancel);
+            return ShowTaskDialog(owner, text, severity, yes: yesCaption, no: noCaption, canCancel: true)
+                ?? ShowMessageBox(owner, text, severity, MessageBoxButtons.YesNoCancel);
         }
 
         /// <summary>
-        /// Asks the user to choose between three options (yes/no/cancel) using a <see cref="MessageBox"/> or <see cref="TaskDialog"/>.
+        /// Asks the user to choose between three options (yes/no/cancel) using a message box or task dialog.
         /// </summary>
         /// <param name="owner">The parent window the displayed window is modal to; can be <c>null</c>.</param>
         /// <param name="text">The message to be displayed.</param>
@@ -244,17 +129,13 @@ namespace NanoByte.Common
         /// <see cref="DialogResult.Cancel"/> otherwise.</returns>
         public static DialogResult YesNoCancel(IWin32Window? owner, [Localizable(true)] string text, MsgSeverity severity)
             => YesNoCancel(owner, text, severity, Resources.Yes, Resources.No);
-        #endregion
 
-        //--------------------//
-
-        #region MessageBox
         /// <summary>Displays a message using a <see cref="MessageBox"/>.</summary>
         /// <param name="owner">The parent window the displayed window is modal to; can be <c>null</c>.</param>
         /// <param name="text">The message to be displayed.</param>
         /// <param name="severity">How severe/important the message is.</param>
         /// <param name="buttons">The buttons the user can click.</param>
-        private static DialogResult ShowMesageBox(IWin32Window? owner, [Localizable(true)] string text, MsgSeverity severity, MessageBoxButtons buttons)
+        private static DialogResult ShowMessageBox(IWin32Window? owner, [Localizable(true)] string text, MsgSeverity severity, MessageBoxButtons buttons)
         {
             // Handle RTL systems
             MessageBoxOptions localizedOptions;
@@ -273,72 +154,75 @@ namespace NanoByte.Common
             // Display MessageDialog
             return MessageBox.Show(owner, text, Application.ProductName, buttons, icon, MessageBoxDefaultButton.Button1, localizedOptions);
         }
-        #endregion
 
-        #region TaskDialog
-        /// <summary>
-        /// Displays a message using a <see cref="TaskDialog"/>.
-        /// </summary>
-        /// <param name="text">The message to be displayed.</param>
-        /// <param name="severity">How severe/important the message is.</param>
-        private static TaskDialog.TaskDialog GetTaskDialog([Localizable(true)] string text, MsgSeverity severity)
+        private static DialogResult? ShowTaskDialog(IWin32Window? owner, string text, MsgSeverity severity, string? yes = null, string? no = null, string? ok = null, string? cancel = null, bool canCancel = false)
         {
-            // Split everything from the second line onwards off from the main text
-            string[] split = text.Replace("\r\n", "\n").Split(new[] {'\n'}, 2);
+            if (!WindowsUtils.IsWindowsVista) return null;
+
+            var icon = severity switch
+            {
+                MsgSeverity.Warn => TaskDialogIcon.Warning,
+                MsgSeverity.Error => TaskDialogIcon.Error,
+                MsgSeverity.Info => TaskDialogIcon.Information,
+                _ => TaskDialogIcon.Information
+            };
+
+            string[] splitText = text.Replace("\r\n", "\n").Split(new[] {'\n'}, 2);
+            string heading = splitText[0];
+            string? details = (splitText.Length == 2) ? splitText[1] : null;
+
+            var buttons = new List<TaskDialogButton>();
+
+            void AddButton(DialogResult result, string? caption)
+            {
+                if (!string.IsNullOrEmpty(caption))
+                    buttons.Add(new TaskDialogButton((int)result, caption.Replace("\r\n", "\n")));
+            }
+
+            AddButton(DialogResult.Yes, yes);
+            AddButton(DialogResult.No, no);
+            AddButton(DialogResult.OK, ok);
+
             var taskDialog = new TaskDialog.TaskDialog
             {
                 PositionRelativeToWindow = true,
                 WindowTitle = Application.ProductName,
-                MainInstruction = split[0]
+                MainIcon = icon,
+                MainInstruction = heading,
+                Content = details
             };
-            if (split.Length == 2) taskDialog.Content = split[1];
 
-            // Handle RTL systems
-            taskDialog.RightToLeftLayout = CultureInfo.CurrentUICulture.TextInfo.IsRightToLeft;
-
-            // Select icon based on message severity
-            switch (severity)
+            if (canCancel)
             {
-                case MsgSeverity.Warn:
-                    taskDialog.MainIcon = TaskDialogIcon.Warning;
-                    taskDialog.AllowDialogCancellation = true;
-                    break;
+                if (string.IsNullOrEmpty(ok))
+                    taskDialog.CommonButtons = TaskDialogCommonButtons.Cancel;
+                else
+                    AddButton(DialogResult.Cancel, cancel);
 
-                case MsgSeverity.Error:
-                    taskDialog.MainIcon = TaskDialogIcon.Error;
-                    taskDialog.AllowDialogCancellation = false; // Real errors shouldn't be easily ESCed away
-                    break;
-
-                default:
-                case MsgSeverity.Info:
-                    taskDialog.MainIcon = TaskDialogIcon.Information;
-                    taskDialog.AllowDialogCancellation = true;
-                    break;
+                // Cancel non-errors with ESC and errors with ENTER
+                if (severity < MsgSeverity.Error) taskDialog.AllowDialogCancellation = true;
+                else taskDialog.DefaultButton = (int) DialogResult.Cancel;
             }
 
-            return taskDialog;
-        }
+            if (buttons.Count != 0)
+            {
+                taskDialog.UseCommandLinks = true;
+                taskDialog.Buttons = buttons.ToArray();
+            }
 
-        /// <summary>
-        /// Displays a <see cref="TaskDialog"/>.
-        /// </summary>
-        /// <param name="taskDialog">The <see cref="TaskDialog"/> to display.</param>
-        /// <param name="owner">The parent window the displayed window is modal to; can be <c>null</c>.</param>
-        /// <returns>Indicates the button the user pressed.</returns>
-        /// <exception cref="BadImageFormatException">The task-dialog DLL could not be loaded.</exception>
-        /// <exception cref="EntryPointNotFoundException">The task-dialog DLL routine could not be called.</exception>
-        private static DialogResult ShowTaskDialog(TaskDialog.TaskDialog taskDialog, IWin32Window? owner)
-        {
-            // Note: If you get an EntryPointNotFoundException here, add this to your application manifest and test outside the IDE:
-            // <dependency>
-            //   <dependentAssembly>
-            //     <assemblyIdentity type="win32" name="Microsoft.Windows.Common-Controls" version="6.0.0.0" processorArchitecture="*" publicKeyToken="6595b64144ccf1df" language="*" />
-            //   </dependentAssembly>
-            // </dependency>
-
-            int result = (owner == null) ? taskDialog.Show() : taskDialog.Show(owner);
-            return (DialogResult)result;
+            try
+            {
+                int result = owner == null ? taskDialog.Show() : taskDialog.Show(owner);
+                return (DialogResult)result;
+            }
+            catch (BadImageFormatException)
+            {
+                return null;
+            }
+            catch (EntryPointNotFoundException)
+            {
+                return null;
+            }
         }
-        #endregion
     }
 }
