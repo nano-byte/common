64c87f047b236c7cd0fae5cf99cb9ebe2f74cd49
Bastian Eicher <bastian@eicher.net>
2021-04-25 10:53:25 +0200

    Added missing XML comments

--- src/Common.WinForms/Controls/ControlExtensions.cs
+++ src/Common.WinForms/Controls/ControlExtensions.cs
@@ -11,6 +11,9 @@ namespace NanoByte.Common.Controls
     /// </summary>
     public static class ControlExtensions
     {
+        /// <summary>
+        /// Executes the given <paramref name="action"/> on the thread that owns this control and blocks until it is complete.
+        /// </summary>
         public static void Invoke(this Control control, Action action)
         {
             #region Sanity checks
@@ -21,6 +24,10 @@ namespace NanoByte.Common.Controls
             control.Invoke(action);
         }
 
+        /// <summary>
+        /// Executes the given <paramref name="action"/> on the thread that owns this control and blocks until it is complete.
+        /// </summary>
+        /// <returns>The return value of the <paramref name="action"/>.</returns>
         public static T Invoke<T>(this Control control, Func<T> action)
         {
             #region Sanity checks
