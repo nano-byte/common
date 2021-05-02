6b1ea7a7d4aee67f166f5253fb318b68b2487c2a
Bastian Eicher <bastian@eicher.net>
2021-05-02 17:19:25 +0200

    Moved StringUtils.Hash() and .*Encode() to EncodingUtils

--- src/UnitTests/StringUtilsTest.cs
+++ src/UnitTests/StringUtilsTest.cs
@@ -1,7 +1,6 @@
 // Copyright Bastian Eicher
 // Licensed under the MIT License
 
-using System.Security.Cryptography;
 using FluentAssertions;
 using Xunit;
 
@@ -97,36 +96,6 @@ namespace NanoByte.Common
             "test1\"test2".EscapeArgument().Should().Be("test1\\\"test2", because: "Quotation marks should be escaped");
             "test1\\\\test2".EscapeArgument().Should().Be("test1\\\\test2", because: "Consecutive slashes without quotation marks should not be escaped");
             "test1\\\"test2".EscapeArgument().Should().Be("test1\\\\\\\"test2", because: "Slashes with quotation marks should be escaped");
-        }
-
-        [Fact]
-        public void TestBase64Utf8Encode()
-        {
-            "".Base64Utf8Encode().Should().Be("");
-            "test".Base64Utf8Encode().Should().Be("dGVzdA==");
-        }
-
-        [Fact]
-        public void TestBase64Utf8Decode()
-        {
-            "".Base64Utf8Decode().Should().Be("");
-            "dGVzdA==".Base64Utf8Decode().Should().Be("test");
-        }
-
-        [Fact]
-        public void TestBase32Encode() => new byte[] {65, 66}.Base32Encode().Should().Be("IFBA");
-
-        [Fact]
-        public void TestBase16Encode() => new byte[] {65, 66}.Base16Encode().Should().Be("4142");
-
-        [Fact]
-        public void TestBase16Decode() => "4142".Base16Decode().Should().Equal(65, 66);
-
-        [Fact]
-        public void TestHash()
-        {
-            const string sha1ForEmptyString = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
-            "".Hash(SHA1.Create()).Should().Be(sha1ForEmptyString);
         }
 
         [Fact]
