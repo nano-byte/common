4522a5d9556c179c1099089cca69dd0b5ce71ae1
Bastian Eicher <bastian@eicher.net>
2021-05-26 21:45:54 +0200

    Added .ToMarshalByRef() extension methods

---
+++ src/Common/Threading/ActionExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+using System;
+
+namespace NanoByte.Common.Threading
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="Action{T}"/>
+    /// </summary>
+    public static class ActionExtensions
+    {
+        /// <summary>
+        /// Wraps a delegate so that it is marshalled by reference when passed via .NET Remoting.
+        /// </summary>
+        public static Action<T> ToMarshalByRef<T>(this Action<T> action)
+            => new ActionByRef<T>(action).Invoke;
+
+        /// <summary>
+        /// A generic wrapper to pass a value by reference when using .NET remoting.
+        /// </summary>
+        private class ActionByRef<T> : MarshalNoTimeout
+        {
+            private readonly Action<T> _action;
+
+            public ActionByRef(Action<T> action)
+            {
+                _action = action ?? throw new ArgumentNullException(nameof(action));
+            }
+
+            public void Invoke(T obj)
+                => _action(obj);
+        }
+    }
+}
---
+++ src/Common/Threading/FuncExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+using System;
+
+namespace NanoByte.Common.Threading
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="Func{TIn,TOut}"/>
+    /// </summary>
+    public static class FuncExtensions
+    {
+        /// <summary>
+        /// Wraps a delegate so that it is marshalled by reference when passed via .NET Remoting.
+        /// </summary>
+        public static Func<TIn, TOut> ToMarshalByRef<TIn, TOut>(this Func<TIn, TOut> func)
+            => new FuncByRef<TIn, TOut>(func).Invoke;
+
+        /// <summary>
+        /// A generic wrapper to pass a value by reference when using .NET remoting.
+        /// </summary>
+        private class FuncByRef<TIn, TOut> : MarshalNoTimeout
+        {
+            private readonly Func<TIn, TOut> _func;
+
+            public FuncByRef(Func<TIn, TOut> func)
+            {
+                _func = func ?? throw new ArgumentNullException(nameof(func));
+            }
+
+            public TOut Invoke(TIn obj)
+                => _func(obj);
+        }
+    }
+}
