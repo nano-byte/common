083a8ce196d5b9c1e344b33da81674e79684b8e3
Bastian Eicher <bastian@eicher.net>
2021-06-02 15:04:23 +0200

    Introduced ShadowingStream

---
+++ src/Common/Streams/ShadowingStream.cs
@@ -0,0 +1,63 @@
+﻿// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+using System.IO;
+using System.Threading;
+
+#if !NET20 && !NET40
+using System.Threading.Tasks;
+#endif
+
+#if !NETFRAMEWORK
+using System;
+#endif
+
+namespace NanoByte.Common.Streams
+{
+    /// <summary>
+    /// Decorator that copies all bytes read from a <see cref="Stream"/> to another <see cref="Stream"/>.
+    /// </summary>
+    public sealed class ShadowingStream : DelegatingStream
+    {
+        private readonly Stream _shadowStream;
+
+        /// <summary>
+        /// Creates a new shadowing stream.
+        /// </summary>
+        /// <param name="underlyingStream">Underlying stream to delegate to. Will be disposed together with this stream.</param>
+        /// <param name="shadowStream">The stream to copy all read bytes to.</param>
+        public ShadowingStream(Stream underlyingStream, Stream shadowStream)
+            : base(underlyingStream)
+        {
+            _shadowStream = shadowStream;
+        }
+
+        /// <inheritdoc/>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int read = base.Read(buffer, offset, count);
+            _shadowStream.Write(buffer, offset, count);
+            return read;
+        }
+
+#if !NET20 && !NET40
+        /// <inheritdoc/>
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            int read = await base.ReadAsync(buffer, offset, count, cancellationToken);
+            await _shadowStream.WriteAsync(buffer, offset, count, cancellationToken);
+            return read;
+        }
+#endif
+
+#if !NETFRAMEWORK
+        /// <inheritdoc/>
+        public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            int read = await base.ReadAsync(buffer, cancellationToken);
+            await _shadowStream.WriteAsync(buffer, cancellationToken);
+            return read;
+        }
+#endif
+    }
+}
---
+++ src/UnitTests/Streams/ShadowingStreamTest.cs
@@ -0,0 +1,33 @@
+﻿// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+using System.IO;
+using FluentAssertions;
+using Xunit;
+
+namespace NanoByte.Common.Streams
+{
+    /// <summary>
+    /// Contains test methods for <see cref="ShadowingStream"/>.
+    /// </summary>
+    public class ShadowingStreamTest
+    {
+        private readonly MemoryStream _underlyingStream = new(new byte[] {0, 1, 2, 3, 4});
+        private readonly MemoryStream _shadowStream = new();
+        private readonly ShadowingStream _stream;
+
+        public ShadowingStreamTest()
+        {
+            _stream = new(_underlyingStream, _shadowStream);
+        }
+
+        [Fact]
+        public void TestRead()
+        {
+            var buffer = new byte[3];
+            _stream.Read(buffer, 0, buffer.Length);
+            buffer.Should().Equal(0, 1, 2);
+            _shadowStream.ReadAll().Should().Equal(0, 1, 2);
+        }
+    }
+}
