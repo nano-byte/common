eb537df5e54536cab105ac0cabf1baf009bf5eaa
Bastian Eicher <bastian@eicher.net>
2021-09-11 12:47:32 +0200

    Added MemberNotNullAttribute and MemberNotNullWhenAttribute backports

--- src/Common/Backports/NullableAttributes.cs
+++ src/Common/Backports/NullableAttributes.cs
@@ -1,7 +1,10 @@
-#if NETFRAMEWORK
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Runtime.CompilerServices;
+using System.Diagnostics.CodeAnalysis;
+
+#if NETFRAMEWORK
 namespace System.Diagnostics.CodeAnalysis
 {
     /// <summary>Specifies that null is allowed as an input even if the corresponding type disallows it.</summary>
@@ -82,9 +85,6 @@ namespace System.Diagnostics.CodeAnalysis
     }
 }
 #else
-using System.Runtime.CompilerServices;
-using System.Diagnostics.CodeAnalysis;
-
 [assembly: TypeForwardedTo(typeof(AllowNullAttribute))]
 [assembly: TypeForwardedTo(typeof(DisallowNullAttribute))]
 [assembly: TypeForwardedTo(typeof(MaybeNullAttribute))]
@@ -94,4 +94,69 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: TypeForwardedTo(typeof(NotNullIfNotNullAttribute))]
 [assembly: TypeForwardedTo(typeof(DoesNotReturnAttribute))]
 [assembly: TypeForwardedTo(typeof(DoesNotReturnIfAttribute))]
+#endif
+
+#if NETFRAMEWORK || NETSTANDARD
+namespace System.Diagnostics.CodeAnalysis
+{
+    /// <summary>Specifies that the method or property will ensure that the listed field and property members have not-<see langword="null"/> values.</summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    public sealed class MemberNotNullAttribute : Attribute
+    {
+        /// <summary>Gets field or property member names.</summary>
+        public string[] Members { get; }
+
+        /// <summary>Initializes the attribute with a field or property member.</summary>
+        /// <param name="member">
+        /// The field or property member that is promised to be not-null.
+        /// </param>
+        public MemberNotNullAttribute(string member) => Members = new[] { member };
+
+        /// <summary>Initializes the attribute with the list of field and property members.</summary>
+        /// <param name="members">
+        /// The list of field and property members that are promised to be not-null.
+        /// </param>
+        public MemberNotNullAttribute(params string[] members) => Members = members;
+    }
+
+    /// <summary>Specifies that the method or property will ensure that the listed field and property members have non-<see langword="null"/> values when returning with the specified return value condition.</summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    public sealed class MemberNotNullWhenAttribute : Attribute
+    {
+        /// <summary>Gets the return value condition.</summary>
+        public bool ReturnValue { get; }
+
+        /// <summary>Gets field or property member names.</summary>
+        public string[] Members { get; }
+
+        /// <summary>Initializes the attribute with the specified return value condition and a field or property member.</summary>
+        /// <param name="returnValue">
+        /// The return value condition. If the method returns this value,
+        /// the associated parameter will not be <see langword="null"/>.
+        /// </param>
+        /// <param name="member">The field or property member that is promised to be not-<see langword="null"/>.</param>
+        public MemberNotNullWhenAttribute(bool returnValue, string member)
+        {
+            ReturnValue = returnValue;
+            Members = new[] { member };
+        }
+
+        /// <summary>Initializes the attribute with the specified return value condition and list of field and property members.</summary>
+        /// <param name="returnValue">
+        /// The return value condition. If the method returns this value,
+        /// the associated parameter will not be <see langword="null"/>.
+        /// </param>
+        /// <param name="members">
+        /// The list of field and property members that are promised to be not-null.
+        /// </param>
+        public MemberNotNullWhenAttribute(bool returnValue, params string[] members)
+        {
+            ReturnValue = returnValue;
+            Members = members;
+        }
+    }
+}
+#else
+[assembly: TypeForwardedTo(typeof(MemberNotNullAttribute))]
+[assembly: TypeForwardedTo(typeof(MemberNotNullWhenAttribute))]
 #endif
