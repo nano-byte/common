8cff71b548d21a42897577390803688905d67fff
Bastian Eicher <bastian@eicher.net>
2021-12-05 13:23:55 +0100

    Introduced JobQueue

---
+++ src/Common/Threading/JobQueue.cs
@@ -0,0 +1,55 @@
+﻿// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+using System;
+using System.Collections.Generic;
+
+namespace NanoByte.Common.Threading
+{
+    /// <summary>
+    /// Runs jobs on a single background thread that is started and stopped on demand.
+    /// </summary>
+    public class JobQueue
+    {
+        private readonly object _lock = new();
+        private readonly Queue<Action> _jobs = new();
+        private bool _threadRunning;
+
+        /// <summary>
+        /// Adds a job to the work queue.
+        /// </summary>
+        public void Enqueue(Action job)
+        {
+            lock (_lock)
+            {
+                _jobs.Enqueue(job);
+
+                if (!_threadRunning)
+                {
+                    _threadRunning = true;
+                    ThreadUtils.StartBackground(Work, name: nameof(JobQueue));
+                }
+            }
+        }
+
+        private void Work()
+        {
+            while (true)
+            {
+                Action job;
+                lock (_lock)
+                {
+                    if (_jobs.Count == 0)
+                    {
+                        _threadRunning = false;
+                        return;
+                    }
+
+                    job = _jobs.Dequeue();
+                }
+
+                job();
+            }
+        }
+    }
+}
---
+++ src/UnitTests/Threading/JobQueueTest.cs
@@ -0,0 +1,25 @@
+﻿// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace NanoByte.Common.Threading
+{
+    public class JobQueueTest
+    {
+        [Fact]
+        public async Task RunsWork()
+        {
+            bool called = false;
+
+            var workQueue = new JobQueue();
+            workQueue.Enqueue(() => called = true);
+
+            await Task.Delay(100);
+
+            called.Should().BeTrue();
+        }
+    }
+}
