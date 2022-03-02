960c351835c836975264d30a8cd75fdab85762eb
Bastian Eicher <bastian@eicher.net>
2022-03-02 14:33:12 +0100

    Improved exception documentation in ProcessLauncherExtensions

--- src/Common/Native/ProcessLauncherExtensions.cs
+++ src/Common/Native/ProcessLauncherExtensions.cs
@@ -1,6 +1,8 @@
 // Copyright Bastian Eicher
 // Licensed under the MIT License
 
+using System.Diagnostics;
+
 namespace NanoByte.Common.Native;
 
 /// <summary>
@@ -14,8 +16,10 @@ public static class ProcessLauncherExtensions
     /// <param name="processLauncher">The sub process.</param>
     /// <param name="arguments">Command-line arguments to launch the process with.</param>
     /// <returns>The process' complete stdout output.</returns>
-    /// <exception cref="IOException">The process could not be launched.</exception>
-    /// <exception cref="ExitCodeException">The process exited with a non-zero exit code.</exception>
+    /// <exception cref="IOException">There was a problem launching the executable.</exception>
+    /// <exception cref="FileNotFoundException">The executable file could not be found.</exception>
+    /// <exception cref="NotAdminException">The target process requires elevation.</exception>
+    /// <exception cref="ExitCodeException">The process exited with a non-zero <see cref="Process.ExitCode"/>.</exception>
     public static string RunAndCapture(this IProcessLauncher processLauncher, params string[] arguments)
         => processLauncher.RunAndCapture(null, arguments);
 
@@ -26,8 +30,10 @@ public static class ProcessLauncherExtensions
     /// <param name="stdinData">Data to the process' stdin right after startup.</param>
     /// <param name="arguments">Command-line arguments to launch the process with.</param>
     /// <returns>The process' complete stdout output.</returns>
-    /// <exception cref="IOException">The process could not be launched.</exception>
-    /// <exception cref="ExitCodeException">The process exited with a non-zero exit code.</exception>
+    /// <exception cref="IOException">There was a problem launching the executable.</exception>
+    /// <exception cref="FileNotFoundException">The executable file could not be found.</exception>
+    /// <exception cref="NotAdminException">The target process requires elevation.</exception>
+    /// <exception cref="ExitCodeException">The process exited with a non-zero <see cref="Process.ExitCode"/>.</exception>
     public static string RunAndCapture(this IProcessLauncher processLauncher, ArraySegment<byte> stdinData, params string[] arguments)
         => processLauncher.RunAndCapture(writer =>
         {
