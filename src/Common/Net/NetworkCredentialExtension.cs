2cc8889ae4b4a74bfd25e1ab83e36432e8507145
Bastian Eicher <bastian@eicher.net>
2022-08-14 15:46:36 +0200

    Added NetworkCredential.ToBasicAuth() extension method

---
+++ src/Common/Net/NetworkCredentialExtension.cs
@@ -0,0 +1,21 @@
+﻿// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+#if NET
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace NanoByte.Common.Net;
+
+/// <summary>
+/// Provides extension methods for <see cref="NetworkCredential"/>s.
+/// </summary>
+public static class NetworkCredentialExtension
+{
+    /// <summary>
+    /// Creates a HTTP basic authentication header from the provided credentials.
+    /// </summary>
+    public static AuthenticationHeaderValue ToBasicAuth(this NetworkCredential credential)
+        => new("Basic", (credential.UserName + ":" + credential.Password).Base64Utf8Encode());
+}
+#endif
