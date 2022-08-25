22cf96b4cfdcdf8ce0096ffe8976888e51229005
Bastian Eicher <bastian@eicher.net>
2022-08-25 13:28:49 +0200

    Introduced DeferredProgress

---
+++ src/Common/Tasks/DeferredProgress.cs
@@ -0,0 +1,45 @@
+﻿// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+namespace NanoByte.Common.Tasks;
+
+/// <summary>
+/// Remembers the latest call made to <see cref="Report"/>. Forwards that call (if any) and all future calls to a target <see cref="IProgress{T}"/> implementation once it is set.
+/// </summary>
+/// <typeparam name="T">The type of progress update value.</typeparam>
+/// <remarks>
+/// If <see cref="Report"/> and <see cref="SetTarget"/> are called on different threads individual progress reports may be lost.
+/// This tradeoff is made intentionally to avoid locking for better performance.
+/// </remarks>
+public class DeferredProgress<T> : IProgress<T>
+{
+    private IProgress<T>? _target;
+    private T? _lastValue;
+    private bool _hasValue;
+
+    /// <summary>
+    /// Sets the target <see cref="IProgress{T}"/> implementation to forward <see cref="Report"/> calls to.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">The target is already set.</exception>
+    public void SetTarget(IProgress<T> target)
+    {
+        if (target == null) throw new ArgumentNullException(nameof(target));
+        if (_target != null) throw new InvalidOperationException("The target is already set.");
+
+        if (_hasValue && _lastValue != null)
+            target.Report(_lastValue);
+
+        _target = target;
+    }
+
+    /// <inheritdoc/>
+    public void Report(T value)
+    {
+        if (_target == null)
+        {
+            _lastValue = value;
+            _hasValue = true;
+        }
+        else _target.Report(value);
+    }
+}
---
+++ src/UnitTests/Tasks/DeferredProgressTest.cs
@@ -0,0 +1,35 @@
+﻿// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+namespace NanoByte.Common.Tasks;
+
+public class DeferredProgressTest
+{
+    [Fact]
+    public void ForwardsToTarget()
+    {
+        var deferred = new DeferredProgress<int>();
+
+        var target = new Mock<IProgress<int>>(MockBehavior.Strict);
+        deferred.SetTarget(target.Object);
+
+        target.Setup(x => x.Report(1));
+        target.Setup(x => x.Report(2));
+        deferred.Report(1);
+        deferred.Report(2);
+        target.VerifyAll();
+    }
+
+    [Fact]
+    public void ForwardsLastRecordedToTarget()
+    {
+        var deferred = new DeferredProgress<int>();
+        deferred.Report(1);
+        deferred.Report(2);
+
+        var target = new Mock<IProgress<int>>(MockBehavior.Strict);
+        target.Setup(x => x.Report(2));
+        deferred.SetTarget(target.Object);
+        target.VerifyAll();
+    }
+}
