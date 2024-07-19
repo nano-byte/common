6b67dbc415a32390bade4e90258b7b353c45324d
Bastian Eicher <bastian@eicher.net>
2024-07-19 23:24:37 +0200

    Use primary constructor

--- src/Common/Net/WindowsCliCredentialProvider.cs
+++ src/Common/Net/WindowsCliCredentialProvider.cs
@@ -10,27 +10,17 @@ namespace NanoByte.Common.Net;
 /// <summary>
 /// Asks the user for <see cref="NetworkCredential"/>s using the Windows Credential Manager command-line interface.
 /// </summary>
+/// <param name="beforePrompt">An optional callback to be invoked right before the user is prompted for credentials</param>
 [SupportedOSPlatform("windows")]
-public class WindowsCliCredentialProvider : WindowsCredentialProvider
+public class WindowsCliCredentialProvider(Action? beforePrompt = null) : WindowsCredentialProvider
 {
-    private readonly Action? _beforePrompt;
-
-    /// <summary>
-    /// Creates a new Windows CLI credential provider.
-    /// </summary>
-    /// <param name="beforePrompt">An optional callback to be invoked right before the user is prompted for credentials</param>
-    public WindowsCliCredentialProvider(Action? beforePrompt = null)
-    {
-        _beforePrompt = beforePrompt;
-    }
-
     /// <inheritdoc/>
     protected override NetworkCredential GetCredential(string target, WindowsCredentialsFlags flags)
     {
         if (flags.HasFlag(WindowsCredentialsFlags.IncorrectPassword))
             Log.Error(string.Format(Resources.InvalidCredentials, target));
 
-        _beforePrompt?.Invoke();
+        beforePrompt?.Invoke();
         return WindowsCredentials.PromptCli(target, flags);
     }
 }
