e70b8e023a4e12f0305faec1b02ee3105c956951
Bastian Eicher <bastian@eicher.net>
2025-08-03 00:38:33 +0200

    Added additional methods to BitwiseUtils

--- src/Common/BitwiseUtils.cs
+++ src/Common/BitwiseUtils.cs
@@ -20,4 +20,76 @@ public static class BitwiseUtils
         for (int i = 0; i < array2.Length; i++) result[i] ^= array2[i];
         return result;
     }
+
+    /// <summary>
+    /// Extracts the low-order word (first 16 bits) of a 32-bit integer.
+    /// </summary>
+    [CLSCompliant(false)]
+    [Pure]
+    public static short LoWord(uint l)
+    {
+        unchecked
+        {
+            return (short)(l & 0xffff);
+        }
+    }
+
+    /// <summary>
+    /// Extracts the low-order word (first 16 bits) of a 32-bit integer.
+    /// </summary>
+    [Pure]
+    public static short LoWord(int l)
+    {
+        unchecked
+        {
+            return (short)(l & 0xffff);
+        }
+    }
+
+    /// <summary>
+    /// Extracts the high-order word (last 16 bits) of a 32-bit integer.
+    /// </summary>
+    [CLSCompliant(false)]
+    [Pure]
+    public static short HiWord(uint l)
+    {
+        unchecked
+        {
+            return (short)(l >> 16);
+        }
+    }
+
+    /// <summary>
+    /// Extracts the high-order word (last 16 bits) of a 32-bit integer.
+    /// </summary>
+    [Pure]
+    public static short HiWord(int l)
+    {
+        unchecked
+        {
+            return (short)(l >> 16);
+        }
+    }
+
+    /// <summary>
+    /// Extracts the high-order nibble (first 4 bits) of a byte.
+    /// </summary>
+    /// <param name="b">The byte to extract from.</param>
+    /// <returns>The high-order nibble as an integer.</returns>
+    [Pure]
+    public static int HiNibble(byte b) => b >> 4;
+
+    /// <summary>
+    /// Extracts the low-order nibble (last 4 bits) of a byte.
+    /// </summary>
+    /// <param name="b">The byte to extract from.</param>
+    /// <returns>The low-order nibble as an integer.</returns>
+    [Pure]
+    public static int LoNibble(byte b) => b & 15;
+
+    /// <summary>
+    /// Combines a high nibble and a low nibble (4 bits) into a single byte.
+    /// </summary>
+    [Pure]
+    public static byte CombineHiLoNibble(int high, int low) => (byte)((high << 4) + low);
 }
