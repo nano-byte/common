15a27650fd5f419fca050d8c1b9f01b81dd4dad2
Bastian Eicher <bastian@eicher.net>
2025-08-04 12:56:13 +0200

    Merged UpdateUtils.To() overloads and added null support

--- src/Common/UpdateUtils.cs
+++ src/Common/UpdateUtils.cs
@@ -43,10 +43,10 @@ public static class UpdateUtils
 #if !NET20 && !NET40
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-    public static void To<T>(this T value, ref T original, ref bool updated) where T : struct
+    public static void To<T>(this T value, ref T original, ref bool updated)
     {
         // If the values already match, nothing needs to be done
-        if (original.Equals(value)) return;
+        if (Equals(original, value)) return;
 
         // Otherwise set the "updated" flag and change the value
         updated = true;
@@ -64,48 +64,10 @@ public static class UpdateUtils
 #if !NET20 && !NET40
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-    public static void To<T>(this T value, ref T original, ref bool updated1, ref bool updated2) where T : struct
+    public static void To<T>(this T value, ref T original, ref bool updated1, ref bool updated2)
     {
         // If the values already match, nothing needs to be done
-        if (original.Equals(value)) return;
-
-        updated1 = true;
-        updated2 = true;
-        original = value;
-    }
-
-    /// <summary>
-    /// Updates a value and sets a boolean flag to <c>true</c> if the original value actually changed
-    /// </summary>
-    /// <param name="value">The new value</param>
-    /// <param name="original">The original value to update</param>
-    /// <param name="updated">Gets set to <c>true</c> if value is different from original</param>
-#if !NET20 && !NET40
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
-    public static void To(this string value, ref string original, ref bool updated)
-    {
-        // If the values already match, nothing needs to be done
-        if (original == value) return;
-
-        // Otherwise set the "updated" flag and change the value
-        updated = true;
-        original = value;
-    }
-
-    /// <summary>
-    /// Updates a value and sets two boolean flags to <c>true</c> if the original value actually changed
-    /// </summary>
-    /// <param name="value">The new value</param>
-    /// <param name="original">The original value to update</param>
-    /// <param name="updated1">Gets set to <c>true</c> if value is different from original</param>
-    /// <param name="updated2">Gets set to <c>true</c> if value is different from original</param>
-#if !NET20 && !NET40
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
-    public static void To(this string value, ref string original, ref bool updated1, ref bool updated2)
-    {
-        if (original == value) return;
+        if (Equals(original, value)) return;
 
         updated1 = true;
         updated2 = true;
@@ -122,54 +84,17 @@ public static class UpdateUtils
 #if !NET20 && !NET40
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-    public static void To<T>(this T value, ref T original, [InstantHandle] Action updated) where T : struct
+    public static void To<T>(this T value, ref T original, [InstantHandle] Action updated)
     {
         #region Sanity checks
         if (updated == null) throw new ArgumentNullException(nameof(updated));
         #endregion
 
         // If the values already match, nothing needs to be done
-        if (original.Equals(value)) return;
+        if (Equals(original, value)) return;
 
         // Backup the original value in case it needs to be reverted
         var backup = original;
-
-        // Set the new value
-        original = value;
-
-        // Execute the "updated" delegate
-        try
-        {
-            updated.Invoke();
-        }
-        catch
-        {
-            // Restore the original value before passing exceptions upwards
-            original = backup;
-            throw;
-        }
-    }
-
-    /// <summary>
-    /// Updates a value and calls back a delegate if the original value actually changed.
-    /// </summary>
-    /// <param name="value">The new value.</param>
-    /// <param name="original">The original value to update.</param>
-    /// <param name="updated">Gets called if value is different from original.</param>
-#if !NET20 && !NET40
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
-    public static void To(this string value, ref string original, [InstantHandle] Action updated)
-    {
-        #region Sanity checks
-        if (updated == null) throw new ArgumentNullException(nameof(updated));
-        #endregion
-
-        // If the values already match, nothing needs to be done
-        if (original == value) return;
-
-        // Backup the original value in case it needs to be reverted
-        string backup = original;
 
         // Set the new value
         original = value;
---
+++ src/UnitTests/UpdateUtilsTest.cs
@@ -0,0 +1,117 @@
+﻿// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+namespace NanoByte.Common;
+
+public class UpdateUtilsTest
+{
+    [Fact]
+    public void To_Function()
+    {
+        Assert.Equal("TEST", "test".To(x => x.ToUpper()));
+    }
+
+    [Fact]
+    public void To_ValueChanged_UpdatesOriginalAndSetsFlag()
+    {
+        const int newValue = 42;
+        int value = 0;
+        bool updated = false;
+
+        newValue.To(ref value, ref updated);
+
+        Assert.True(updated);
+        Assert.Equal(newValue, value);
+    }
+
+    [Fact]
+    public void To_ValueChanged_UpdatesOriginalAndSetsFlag_Null()
+    {
+        string? newValue = null;
+        string? value = "value";
+        bool updated = false;
+
+        newValue.To(ref value, ref updated);
+
+        Assert.True(updated);
+        Assert.Equal(newValue, value);
+    }
+
+    [Fact]
+    public void To_ValueDidNotChange_DoesNotUpdateOriginalOrFlag()
+    {
+        const int newValue = 42;
+        int value = 42;
+        bool updated = false;
+
+        newValue.To(ref value, ref updated);
+
+        Assert.False(updated);
+        Assert.Equal(42, value);
+    }
+
+    [Fact]
+    public void To_TwoFlags_BothFlagsSetIfValueChanged()
+    {
+        const int newValue = 42;
+        int value = 0;
+        bool updated1 = false, updated2 = false;
+
+        newValue.To(ref value, ref updated1, ref updated2);
+
+        Assert.True(updated1);
+        Assert.True(updated2);
+        Assert.Equal(newValue, value);
+    }
+
+    [Fact]
+    public void To_TwoFlags_NoFlagsSetIfValueDidNotChange()
+    {
+        const int newValue = 42;
+        int value = 42;
+        bool updated1 = false, updated2 = false;
+
+        newValue.To(ref value, ref updated1, ref updated2);
+
+        Assert.False(updated1);
+        Assert.False(updated2);
+        Assert.Equal(42, value);
+    }
+
+    [Fact]
+    public void To_ValueChanged_ExecutesCallback()
+    {
+        const int newValue = 42;
+        int value = 0;
+        bool callbackExecuted = false;
+
+        newValue.To(ref value, () => callbackExecuted = true);
+
+        Assert.True(callbackExecuted);
+        Assert.Equal(newValue, value);
+    }
+
+    [Fact]
+    public void To_ValueChanged_ExecutesCallback_Null()
+    {
+        string? newValue = null;
+        string? value = "value";
+        bool callbackExecuted = false;
+
+        newValue.To(ref value, () => callbackExecuted = true);
+
+        Assert.True(callbackExecuted);
+        Assert.Equal(newValue, value);
+    }
+
+    [Fact]
+    public void Swap()
+    {
+        int value1 = 10, value2 = 20;
+
+        UpdateUtils.Swap(ref value1, ref value2);
+
+        Assert.Equal(20, value1);
+        Assert.Equal(10, value2);
+    }
+}
