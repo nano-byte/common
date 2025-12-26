9ec1d19f6378aac327645e1fed3951a2ad6d6d0c
Bastian Eicher <bastian@eicher.net>
2025-12-26 11:34:30 +0100

    Added TimeSpanUtils

---
+++ src/Common/TimeSpanUtils.cs
@@ -0,0 +1,51 @@
+// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+namespace NanoByte.Common;
+
+/// <summary>
+/// Helper methods for <see cref="TimeSpan"/>.
+/// </summary>
+public static class TimeSpanUtils
+{
+    /// <summary>
+    /// Like <see cref="TimeSpan.FromSeconds(double)"/>, but with higher-than-millisecond accuracy.
+    /// </summary>
+    public static TimeSpan FromSecondsAccurate(double seconds)
+#if NETFRAMEWORK
+        => TimeSpan.FromTicks((long)(seconds * TimeSpan.TicksPerSecond));
+#else
+        => TimeSpan.FromSeconds(seconds);
+#endif
+
+    /// <summary>
+    /// Multiplies the timespan by a factor.
+    /// </summary>
+    public static TimeSpan Multiply(this TimeSpan timeSpan, double factor)
+#if NETFRAMEWORK
+        => TimeSpan.FromTicks((long)(timeSpan.Ticks * factor));
+#else
+        => timeSpan * factor;
+#endif
+
+    /// <summary>
+    /// Divides the timespan by a factor.
+    /// </summary>
+    public static TimeSpan Divide(this TimeSpan timeSpan, double factor)
+#if NETFRAMEWORK
+        => TimeSpan.FromTicks((long)(timeSpan.Ticks / factor));
+#else
+        => timeSpan / factor;
+#endif
+
+    /// <summary>
+    /// Divides two timespans, returning the quotient.
+    /// </summary>
+    public static double Divide(this TimeSpan timeSpan, TimeSpan other)
+#if NETFRAMEWORK
+        => timeSpan.Ticks / (double)other.Ticks;
+#else
+        => timeSpan / other;
+#endif
+}
+
---
+++ src/UnitTests/TimeSpanUtilsTest.cs
@@ -0,0 +1,42 @@
+﻿// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+namespace NanoByte.Common;
+
+public class TimeSpanUtilsTests
+{
+    [Theory]
+    [InlineData(1)] // second accuracy
+    [InlineData(1.2345)] // millisecond accuracy
+    [InlineData(1.2345678)] // sub-millisecond accuracy
+    public void FromSecondsAccurate(double seconds)
+    {
+        var result = TimeSpanUtils.FromSecondsAccurate(seconds);
+        result.TotalSeconds.Should().Be(seconds);
+    }
+
+    [Fact]
+    public void Multiply()
+    {
+        var ts = TimeSpan.FromSeconds(10);
+        var result = ts.Multiply(2.5);
+        result.Should().Be(TimeSpan.FromSeconds(25));
+    }
+
+    [Fact]
+    public void DivideFactor()
+    {
+        var ts = TimeSpan.FromSeconds(10);
+        var result = TimeSpanUtils.Divide(ts, 2);
+        result.Should().Be(TimeSpan.FromSeconds(5));
+    }
+
+    [Fact]
+    public void DivideTimeSpans()
+    {
+        var ts1 = TimeSpan.FromSeconds(2);
+        var ts2 = TimeSpan.FromSeconds(10);
+        double result = ts1.Divide(ts2);
+        result.Should().Be(0.2);
+    }
+}
