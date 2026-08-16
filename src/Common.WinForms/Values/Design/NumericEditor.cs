6503af5f89df49016693a2dfa577ad64cb509b9f
Bastian Eicher <bastian@eicher.net>
2026-08-16 16:19:37 +0200

    Added NumericEditor and SliderEditor helpers for PropertyGrid

---
+++ src/Common.WinForms/Values/Design/NumericEditor.cs
@@ -0,0 +1,77 @@
+﻿// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing.Design;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace NanoByte.Common.Values.Design;
+
+/// <summary>
+/// Abstract base class for drop-down <see cref="PropertyGrid"/> editors that can be associated with <c>float</c> or <c>double</c> properties.
+/// </summary>
+public abstract class NumericEditor : UITypeEditor
+{
+    /// <inheritdoc/>
+    public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext? context) => UITypeEditorEditStyle.DropDown;
+
+    /// <inheritdoc/>
+    public override object EditValue(ITypeDescriptorContext? context, IServiceProvider? provider, object? value)
+    {
+        #region Sanity checks
+        if (context == null) throw new ArgumentNullException(nameof(context));
+        if (provider == null) throw new ArgumentNullException(nameof(provider));
+        if (value == null) throw new ArgumentNullException(nameof(value));
+        #endregion
+
+        if (!TryGetDouble(value, out double doubleValue) || provider.GetService(typeof(IWindowsFormsEditorService)) is not IWindowsFormsEditorService editorService) return value;
+
+        var range = context.PropertyDescriptor?.Attributes.OfType<RangeAttribute>().FirstOrDefault();
+        double newValue = (range == null)
+            ? EditValue(doubleValue, editorService)
+            : EditValue(doubleValue, range, editorService);
+        if (value is float)
+            return (float)newValue;
+        else
+            return newValue;
+    }
+
+    /// <summary>
+    /// Displays the UI to edit the <c>float</c> value.
+    /// </summary>
+    /// <param name="value">The current value.</param>
+    /// <param name="editorService">The editor service used to display the dropdown control.</param>
+    /// <returns>The value set by the user.</returns>
+    protected abstract double EditValue(double value, IWindowsFormsEditorService editorService);
+
+    /// <summary>
+    /// Displays the UI to edit the <c>float</c> value.
+    /// </summary>
+    /// <param name="value">The current value.</param>
+    /// <param name="range">The range of valid values the user can select.</param>
+    /// <param name="editorService">The editor service used to display the dropdown control.</param>
+    /// <returns>The value set by the user.</returns>
+    protected abstract double EditValue(double value, RangeAttribute range, IWindowsFormsEditorService editorService);
+
+    /// <summary>
+    /// Interprets <paramref name="value"/> as a <c>double</c> if it is a <c>float</c> or <c>double</c>.
+    /// </summary>
+    protected bool TryGetDouble(object? value, out double doubleValue)
+    {
+        switch (value)
+        {
+            case double d:
+                doubleValue = d;
+                return true;
+            case float f:
+                doubleValue = f;
+                return true;
+            default:
+                doubleValue = 0;
+                return false;
+        }
+    }
+}
---
+++ src/Common.WinForms/Values/Design/SliderEditor.cs
@@ -0,0 +1,39 @@
+﻿// Copyright Bastian Eicher
+// Licensed under the MIT License
+
+using System.ComponentModel.DataAnnotations;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace NanoByte.Common.Values.Design;
+
+/// <summary>
+/// An editor that can be associated with <c>float</c> or <c>double</c> properties to provide a <see cref="TrackBar"/> interface.
+/// </summary>
+public class SliderEditor : NumericEditor
+{
+    /// <inheritdoc/>
+    protected override double EditValue(double value, IWindowsFormsEditorService editorService)
+        => EditValue(value, new(0, 10), editorService);
+
+    /// <inheritdoc/>
+    protected override double EditValue(double value, RangeAttribute range, IWindowsFormsEditorService editorService)
+    {
+        #region Sanity checks
+        if (editorService == null) throw new ArgumentNullException(nameof(editorService));
+        if (range == null) throw new ArgumentNullException(nameof(range));
+        #endregion
+
+        // Scale up by factor 40 and clamp within [minimum,maximum]
+        var trackBar = new TrackBar
+        {
+            TickFrequency = 40,
+            Minimum = (int)(Convert.ToDouble(range.Minimum) * 40),
+            Maximum = (int)(Convert.ToDouble(range.Maximum) * 40),
+        };
+        trackBar.Value = ((int)(value * 40)).Clamp(trackBar.Minimum, trackBar.Maximum);
+
+        editorService.DropDownControl(trackBar);
+        return trackBar.Value / 40f;
+    }
+}
